4bdbad7bbd08b267554e5db08bdad7c3fc2c58eb
313jesse <jess7642@gmail.com>
2018-08-02 16:06:52 -0400

    Committing to the beginning

--- MidtermProjects/Game.cs
+++ MidtermProjects/Game.cs
@@ -8,5 +8,6 @@ namespace MidtermProjects
 {
     class Game
     {
+        int cw = 0;
     }
 }
--- MidtermProjects/Program.cs
+++ MidtermProjects/Program.cs
@@ -10,6 +10,7 @@ namespace MidtermProjects
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
