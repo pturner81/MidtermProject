ee6c4ee55b1f382c37b9ac0bb4e3628c39358c8e
TerrellWhite <Terrell.White053100@gmail.com>
2018-08-02 17:09:50 -0400

    Loop base

--- MidtermProjects/Program.cs
+++ MidtermProjects/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace MidtermProjects
@@ -10,7 +11,34 @@ namespace MidtermProjects
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Which category would you like to buy from? (Toys, Books, Games)");
+            string cat = Console.ReadLine();
+            bool goAgain = true;
+           //makes sure the user puts in the right input 
+            while (goAgain) 
+            {
+                if (cat.ToLower() == "toys" || cat.ToLower() == "toy")//shows toys
+                {
+                    goAgain = false;
+                }
+                else if (cat.ToLower() == "books" || cat.ToLower() == "book")//shows books
+                {
+                   goAgain = false;
+                }
+                else if (cat.ToLower() == "games"|| cat.ToLower() == "game")//shows game
+                {
+                    goAgain = false;
+                }
+                else //light validation
+                {
+                    while (!(Regex.IsMatch(cat, "^[tT][oO][yY][sS]|[tT][oO][yY]|[bB][oO][oO][kK]|[bB][oO][oO][kK][sS]|[bB][oO][oO][kK]|[gG][aA][mM][eE]|[gG][aA][mM][eE][sS]$")))
+                    {
+                        Console.WriteLine("Enter the right input");
+                        cat = Console.ReadLine();
+                        goAgain = true;
+                    }
+                }
+}
         }
     }
 }
