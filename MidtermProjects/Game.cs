f7e38a8c0e8609a370f600967bbb04f7c6fd1289
313jesse <jess7642@gmail.com>
2018-08-03 16:29:08 -0400

    Added Games and Toys

--- MidtermProjects/Game.cs
+++ MidtermProjects/Game.cs
@@ -6,8 +6,41 @@ using System.Threading.Tasks;
 
 namespace MidtermProjects
 {
-    class Game
+    class Game : Products
+
+{
+    public Game(string nam, string des, string tit, string ite, double pri) :
+            base(nam, des, tit, ite, pri)
+
     {
-        int cw = 0;
+        Console.WriteLine($"Name: {NAME}, Description: {Description}, Title: {Title}, Item Number: {ItemNum}, Price:{Price:C}");
     }
+
+    public void PrintInfo1()
+    {
+
+        Game prod1, prod2, prod3;
+
+        prod1 = new Game("UNO", "No description here", "UNO Classic", "234234234535", 6.99);
+        prod2 = new Game("Poker Set", "52 Cards Chips and Dealer Button", "Hoyles Classic Poker Set", "4243364848293", 46.99);
+        prod3 = new Game("Cards Against Humanity", " The Starter Deck", "Cards Against Humanity Starter Deck", "27245337483746", 24.99);
+
+        List<Game> products = new List<Game>() { prod1, prod2, prod3 };
+    }
+
+
+
+    public void PrintList(List<Game> products)
+    {
+        foreach (Game prod in products)
+        {
+            PrintList(products);
+        }
+    }
+
 }
+
+
+
+}
+
--- MidtermProjects/Toy.cs
+++ MidtermProjects/Toy.cs
@@ -6,13 +6,39 @@ using System.Threading.Tasks;
 
 namespace MidtermProjects
 {
-    class Toy
+    class Toy : Products
     {
+            public Toy(string nam, string des, string tit, string ite, double pri) :
+                    base(nam, des, tit, ite, pri)
 
-        List<string> Toys = new List<string>();
+            {
+                Console.WriteLine($"Name: {NAME}, Description: {Description}, Title: {Title}, Item Number: {ItemNum}, Price:{Price:C}");
+            }
 
-        
+            public void PrintInfo1()
+            {
 
-        
+                Toy prod1, prod2, prod3;
+
+                prod1 = new Toy("Pikachu Plush", "14 inch plush Pikachu", "Pika Pika Pakachu", "47438347282374", 24.67);
+                prod2 = new Toy("Batman Bobblehead", "4 inch Batman Begins Bobblehead", "BatBobble", "464247842848293", 19.99);
+                prod3 = new Toy("Superman 2 Hero", " Classic superman action figure with opposable thumbs", "Superman action", "23435454746", 27.99);
+
+                List<Toy> products = new List<Toy>() { prod1, prod2, prod3 };
+            }
+
+
+
+            public void PrintList(List<Toy> products)
+            {
+                foreach (Toy prod in products)
+                {
+                    PrintList(products);
+                }
+            }
+
+        }
+
+
+
     }
-}
