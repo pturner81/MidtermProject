bb738f21a01774e221d43aea3df9a115c9de7223
Turner <turnerp81@gmail.com>
2018-08-03 16:19:58 -0400

    String Validation

--- MidtermProjects/Program.cs
+++ MidtermProjects/Program.cs
@@ -11,10 +11,8 @@ namespace MidtermProjects
     {
         static void Main(string[] args)
         {
-            Instantiate();
-
             Console.WriteLine("Which category would you like to buy from? (Toys, Books, Games)");
-            string cat = Console.ReadLine();
+            string cat = ValidateString(Console.ReadLine());
             bool goAgain = true;
             //makes sure the user puts in the right input
             //list cart imported from the cart class
@@ -22,7 +20,7 @@ namespace MidtermProjects
             while (goAgain)
             {
                 //if printagain = 1 cw("Pick another category") takes in cat value and runs through again
-                if (cat.ToLower() == "toys" || cat.ToLower() == "toy")//shows toys
+                if (cat == "toys" || cat == "toy")//shows toys
                 {
                     //show toys, ask if they would like to add any to cart 
                     //while addtocart == true
@@ -31,7 +29,7 @@ namespace MidtermProjects
                     //if yes then printagain = 1 and goAgain = true the program ask for user to input another catgory
                     goAgain = false;// if they are done adding items to the cart and would like to go to check out
                 }
-                else if (cat.ToLower() == "books" || cat.ToLower() == "book")//shows books
+                else if (cat == "books" || cat == "book")//shows books
                 {
 
 
@@ -42,7 +40,7 @@ namespace MidtermProjects
                     //if yes then  printagain = 1 and goAgain = true the program ask for user to input another catgory
                     goAgain = false;// if they are done adding items to the cart and would like to go to check out
                 }
-                else if (cat.ToLower() == "games" || cat.ToLower() == "game")//shows game
+                else if (cat == "games" || cat == "game")//shows game
                 {
 
                     //show games, ask if they would like to add any to cart 
@@ -65,23 +63,30 @@ namespace MidtermProjects
             //checkout code 
         }
 
-        private static void Instantiate()
-        {
-            Book prod1, prod2, prod3;
-
-            prod1 = new Book("Venom meets Carnage", "volume 1 issue 12 47 pages.", "Venom\'s Revenge", "234535", 5.99);
-            prod2 = new Book("The beginning of Wolverine", "Volume 1 issue 1", "Wolverine Genesis", "464848293", 6.99);
-            prod3 = new Book("Tacos and Mercs", " Volume 3 issue 24", "Deadpool lives", "2737483746", 11.99);
-
-            List<Book> products = new List<Book>() { prod1, prod2, prod3 };
-        }
-
         public void PrintList(List<Book> products)
         {
             foreach (Book prod in products)
             {
                 PrintList(products);
                 Console.WriteLine();
+            }
+        }
+        public static string ValidateString(string UserInput)
+        {
+            try
+            {
+                UserInput = UserInput.ToLower();
+                return (UserInput);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+                return "0";
+            }
+            catch (Exception f)
+            {
+                Console.WriteLine(f.Message);
+                return "0";
             }
         }
     }
