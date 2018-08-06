a984058908f47531ef8ba47c772c9ddcbf089161
TerrellWhite <Terrell.White053100@gmail.com>
2018-08-06 10:11:42 -0400

    Test

--- MidtermProjects/Program.cs
+++ MidtermProjects/Program.cs
@@ -11,68 +11,84 @@ namespace MidtermProjects
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Which category would you like to buy from? (Toys, Books, Games)");
-            string cat = Validators.ValidateString(Console.ReadLine());
-            cat = Validators.IsOption(cat);
+           
+        //    Console.WriteLine("Which category would you like to buy from? (Toys, Books, Games)");
+        //    string cat = ValidateString(Console.ReadLine());
+        //    bool goAgain = true;
+        //    //makes sure the user puts in the right input
+        //    //list cart imported from the cart class
+        //    //printagain = 0 
+        //    while (goAgain)
+        //    {
+        //        //if printagain = 1 cw("Pick another category") takes in cat value and runs through again
+        //        if (cat == "toys" || cat == "toy")//shows toys
+        //        {
+        //            //show toys, ask if they would like to add any to cart 
+        //            //while addtocart == true
+        //            //ask if they would like to add anything else to cart 
+        //            //outside the loop it asks if they want to go to another category
+        //            //if yes then printagain = 1 and goAgain = true the program ask for user to input another catgory
+        //            goAgain = false;// if they are done adding items to the cart and would like to go to check out
+        //        }
+        //        else if (cat == "books" || cat == "book")//shows books
+        //        {
 
-            bool goAgain = true;
-            //makes sure the user puts in the right input
-            //list cart imported from the cart class
-            //printagain = 0 
-            while (goAgain)
-            {
-                //if printagain = 1 cw("Pick another category") takes in cat value and runs through again
-                if (cat == "toys")//shows toys
-                {
-                    //show toys, ask if they would like to add any to cart 
-                    //while addtocart == true
-                    //ask if they would like to add anything else to cart 
-                    //outside the loop it asks if they want to go to another category
-                    //if yes then printagain = 1 and goAgain = true the program ask for user to input another catgory
-                    goAgain = false;// if they are done adding items to the cart and would like to go to check out
-                }
-                else if (cat == "books")//shows books
-                {
 
+        //            //show books, ask if they would like to add any to cart 
+        //            //while addtocart == true
+        //            //ask if they would like to add anything else to cart 
+        //            //outside the loop it asks if they want to go to another category
+        //            //if yes then  printagain = 1 and goAgain = true the program ask for user to input another catgory
+        //            goAgain = false;// if they are done adding items to the cart and would like to go to check out
+        //        }
+        //        else if (cat == "games" || cat == "game")//shows game
+        //        {
 
-                    //show books, ask if they would like to add any to cart 
-                    //while addtocart == true
-                    //ask if they would like to add anything else to cart 
-                    //outside the loop it asks if they want to go to another category
-                    //if yes then  printagain = 1 and goAgain = true the program ask for user to input another catgory
-                    goAgain = false;// if they are done adding items to the cart and would like to go to check out
-                }
-                else if (cat == "games")//shows game
-                {
+        //            //show games, ask if they would like to add any to cart 
+        //            //while addtocart == true
+        //            //ask if they would like to add anything else to cart 
+        //            //outside the loop it asks if they want to go to another category
+        //            //if yes then printagain = 1 and goAgain = true the program ask for user to input another catgory
+        //            goAgain = false;// if they are done adding items to the cart and would like to go to check out
+        //        }
+        //        else //light validation
+        //        {
+        //            while (!(Regex.IsMatch(cat, "^[tT][oO][yY][sS]|[tT][oO][yY]|[bB][oO][oO][kK]|[bB][oO][oO][kK][sS]|[bB][oO][oO][kK]|[gG][aA][mM][eE]|[gG][aA][mM][eE][sS]$")))
+        //            {
+        //                Console.WriteLine("Enter the right input");
+        //                cat = Console.ReadLine();
+        //                goAgain = true;
+        //            }
+        //        }
+        //    }
+        //    //checkout code 
+        //}
 
-                    //show games, ask if they would like to add any to cart 
-                    //while addtocart == true
-                    //ask if they would like to add anything else to cart 
-                    //outside the loop it asks if they want to go to another category
-                    //if yes then printagain = 1 and goAgain = true the program ask for user to input another catgory
-                    goAgain = false;// if they are done adding items to the cart and would like to go to check out
-                }
-                else //light validation
-                {
-                    while (!(Regex.IsMatch(cat, "^[tT][oO][yY][sS]|[tT][oO][yY]|[bB][oO][oO][kK]|[bB][oO][oO][kK][sS]|[bB][oO][oO][kK]|[gG][aA][mM][eE]|[gG][aA][mM][eE][sS]$")))
-                    {
-                        Console.WriteLine("Enter the right input");
-                        cat = Console.ReadLine();
-                        goAgain = true;
-                    }
-                }
-            }
-            //checkout code 
+        //public void PrintList(List<Book> products)
+        //{
+        //    foreach (Book prod in products)
+        //    {
+        //        PrintList(products);
+        //        Console.WriteLine();
+        //    }
+        //}
+        //public static string ValidateString(string UserInput)
+        //{
+        //    try
+        //    {
+        //        UserInput = UserInput.ToLower();
+        //        return (UserInput);
+        //    }
+        //    catch (FormatException e)
+        //    {
+        //        Console.WriteLine(e.Message);
+        //        return "0";
+        //    }
+        //    catch (Exception f)
+        //    {
+        //        Console.WriteLine(f.Message);
+        //        return "0";
+        //    }
         }
-
-        public void PrintList(List<Book> products)
-        {
-            foreach (Book prod in products)
-            {
-                PrintList(products);
-                Console.WriteLine();
-            }
-        }
-
     }
 }
