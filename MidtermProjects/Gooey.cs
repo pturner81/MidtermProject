2c653fbe8d805e7b15dedc21505c6640ea87e8b2
313jesse <jess7642@gmail.com>
2018-08-07 11:57:32 -0400

    GUI new menu

--- MidtermProjects/Gooey.cs
+++ MidtermProjects/Gooey.cs
@@ -16,5 +16,15 @@ namespace MidtermProjects
         {
             InitializeComponent();
         }
+
+        private void newArrivalsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void marvelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
